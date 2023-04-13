663a4f4eb0b0752953cc3a9200913dc82700672d
razvan404 <razvanpetec@gmail.com>
2023-04-13 13:05:38 +0300

    handling post requests

--- MusicLobby/api/models.py
+++ MusicLobby/api/models.py
@@ -11,7 +11,7 @@ def generate_unique_code():
 
 # Create your models here.
 class Room(models.Model):
-    code = models.CharField(max_length=8, default='', unique=True)
+    code = models.CharField(max_length=8, default=generate_unique_code, unique=True)
     host = models.CharField(max_length=15, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null = False, default=1)
